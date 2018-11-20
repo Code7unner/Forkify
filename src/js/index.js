c41e8876dabd470897090d124aa90b742a4dbee3
Soul7ucker <cherbadzhimaxim@gmail.com>
2018-11-20 19:43:07 +1000

    Fix bug with drawing recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,54 @@
-// Global app controller
+//Global app controller
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from "./views/base";
+import {elements, renderLoader, clearLoader} from './views/base';
 
 //Global state of the app
 const state = {};
 
+/**
+ * SEARCH CONTROLLER
+**/
 const controlSearch = async () => {
+    //Get query from view
     const query = searchView.getInput();
 
     if (query) {
+        //New search object and add to state
         state.search = new Search(query);
 
         //Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
-        //Search the results
-        await state.search.getResults();
+        try {
+            //Search for recipes
+            await state.search.getResults();
 
-        //Render results on UI
-        searchView.renderResults(state.search.result);
+            //Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+
+        } catch (err) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
     }
-
 };
 
 elements.searchForm.addEventListener('submit', e => {
      e.preventDefault();
      controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
 });
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -6,7 +6,7 @@ export default class Search {
     }
 
     async getResults(query) {
-        const proxy = 'https://cors-anywhere.herokuapp.com/'; //'https://crossorigin.me/'
+        const proxy = 'https://cors-anywhere.herokuapp.com/'; //'https://crossorigin.me/';
         const key = '462b1cc8d4f2730081462fbc65136320';  //'25a088f27ffcc8489389a5a22ee88553';
 
         try {
